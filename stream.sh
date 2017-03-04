8ccc446f8f8dd4eb5bce6760619e3aba90fd200c
hiranmoy <hiranmoy.iitkgp@gmail.com>
2017-03-04 21:23:08 +0530

    First month anniversary commit on copyright description

--- stream.sh
+++ stream.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+
+# -*- Visual basic -*-
+
+#*****************************************************************
+#
+#        Copyright 2017 Hiranmoy Basak
+#
+#                  All Rights Reserved.
+#
+#           THIS WORK CONTAINS TRADE SECRET And
+#       PROPRIETARY INFORMATION WHICH Is THE PROPERTY
+#            OF HIRANMOY BASAK OR ITS LICENSOR
+#            AND IS SUBJECT TO LICENSE TERMS.
+#
+#*****************************************************************/
+#
+# No part of this file may be reproduced, stored in a retrieval system,
+# Or transmitted in any form Or by any means --- electronic, mechanical,
+# photocopying, recording, Or otherwise --- without prior written permission
+# of Hiranmoy Basak.
+#
+# WARRANTY:
+# Use all material in this file at your own risk. Hiranmoy Basak.
+# makes no claims about any material contained in this file.
+# 
+# Author: Hiranmoy Basak (hiranmoy.iitkgp@gmail.com)
+
+
+
 scriptProcessIdFile="/home/pi/automation/dump/stream_script.process"
 
 LD_LIBRARY_PATH=/usr/local/lib mjpg_streamer -i "input_file.so -f /tmp/stream -n pic.jpg" -o "output_http.so -w /usr/local/www" &
--- utils.py
+++ utils.py
@@ -1,5 +1,33 @@
 #!/usr/bin/python
 
+
+# -*- Visual basic -*-
+
+#*****************************************************************
+#
+#        			 Copyright 2017 Hiranmoy Basak
+#
+#                  All Rights Reserved.
+#
+#           THIS WORK CONTAINS TRADE SECRET And
+#       PROPRIETARY INFORMATION WHICH Is THE PROPERTY
+#            OF HIRANMOY BASAK OR ITS LICENSOR
+#            AND IS SUBJECT TO LICENSE TERMS.
+#
+#*****************************************************************/
+#
+# No part of this file may be reproduced, stored in a retrieval system,
+# Or transmitted in any form Or by any means --- electronic, mechanical,
+# photocopying, recording, Or otherwise --- without prior written permission
+# of Hiranmoy Basak.
+#
+# WARRANTY:
+# Use all material in this file at your own risk. Hiranmoy Basak.
+# makes no claims about any material contained in this file.
+# 
+# Author: Hiranmoy Basak (hiranmoy.iitkgp@gmail.com)
+
+
 gAddMotionSensor = 0
 gAddCamera = 0
 gAddLightings = 0
